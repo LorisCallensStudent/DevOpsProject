53e453bfd02c9fdf155a4c8f6695550a809c2ed2
unknown <matthis.vandesompele@student.vives.be>
2021-01-09 11:56:27 +0100

    aanpassingen restart

--- Provisionscipts/win_10_engineer.sh
+++ Provisionscipts/win_10_engineer.sh
@@ -2,7 +2,8 @@ scoop bucket add github-gh https://github.com/cli/scoop-gh.git
 scoop install gh
 powershell
 
-Rename-Computer -NewName computerNaam -DomainCredential domain -Restart
+
 $Path = $env:TEMP; $Installer = "putty-64bit-0.74-installer.msi"; Invoke-WebRequest "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.74-installer.msi" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
 $Path = $env:TEMP; $Installer = "Tftpd64-4.64-setup.exe"; Invoke-WebRequest "https://bitbucket.org/phjounin/tftpd64/downloads/Tftpd64-4.64-setup.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
 $Path = $env:TEMP; $Installer = "Firefox Installer.exe"; Invoke-WebRequest "https://www.mozilla.org/nl/firefox/download/thanks/" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+Rename-Computer -NewName computerNaam -DomainCredential domain -Restart
